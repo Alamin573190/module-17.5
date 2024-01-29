50cdb42100c314c2f8c338e8d6f3b84e75ef8f37
Alamin <mdalaminhossain573190@gmail.com>
2024-01-29 13:33:49 +0600

    slice-splice

---
+++ slice.js
@@ -0,0 +1,7 @@
+var numbers = [30 ,50 ,90 ,23 ,65 ,80,10] ;
+// numbers.splice(2,1);
+// numbers.splice(2,0,100,200);
+numbers.splice(2,4,100,200,300,400);
+console.log(numbers);
+// console.log(numbers.slice(1,4));
+// console.log(numbers.splice(0,3));
